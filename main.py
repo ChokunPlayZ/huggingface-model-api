c00fcda1bb7982c5615f011a671991abc130e03e
Hobospider132 <100825450+Hobospider132@users.noreply.github.com>
2023-03-03 19:30:53 +1100

    Update main.py

--- main.py
+++ main.py
@@ -21,9 +21,6 @@ def talk():
     data = request.get_json()
     new_user_input_ids = tokenizer.encode(data["text"] + tokenizer.eos_token, return_tensors='pt')
     
-    # append the new user input tokens to the chat history
-    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
-    
     # generate a response while limiting the total chat history to 1000 tokens
     chat_history_ids = model.generate(
         bot_input_ids, max_length=1000,
@@ -35,6 +32,9 @@ def talk():
         temperature=0.6,
         repetition_penalty=1.3
     )
+    
+    # append the new user input tokens to the chat history
+    bot_input_ids = torch.cat([chat_history_ids, new_user_input_ids], dim=-1) if step > 0 else new_user_input_ids
     
     step += 1  # increment step by 1
     
