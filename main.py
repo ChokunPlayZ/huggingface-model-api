31e9a81e3bb110ea7a246840a1ab0edea9d65580
Chokun Rojanapron <chokun@chokunplayz.com>
2023-04-15 22:47:57 +0700

    update generate code for more accruacy

--- main.py
+++ main.py
@@ -12,16 +12,18 @@ app = Flask('')
 def generate_response(prompt):
     input_ids = tokenizer.encode(prompt + tokenizer.eos_token, return_tensors="pt")
     response = model.generate(
-        input_ids=input_ids, 
-        max_length=1000, 
-        pad_token_id=tokenizer.eos_token_id,
-        no_repeat_ngram_size=3,       
-        do_sample=True, 
-        top_k=100, 
-        top_p=0.7,
-        temperature=0.6,
-        repetition_penalty=1.3
-        )
+        model.generate(
+      input_ids=input_ids, 
+      max_length=1000,
+      pad_token_id=tokenizer.eos_token_id,  
+      no_repeat_ngram_size=3,       
+      do_sample=True, 
+      top_k=100, 
+      top_p=0.7,
+      temperature=0.8,
+      repetition_penalty = 1.3
+    )
+    )
     response_text = tokenizer.decode(response[0], skip_special_tokens=True)
     response_text = response_text.replace(prompt, "").strip() # Remove the prompt text from the response
     return response_text
